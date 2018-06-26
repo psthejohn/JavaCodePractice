699403585a38418c8037c7604e4e70bdd1c9a261
psthejohn <36690315+psthejohn@users.noreply.github.com>
2018-06-27 00:13:12 +0530

    Update MatrixDiagonalDifference.java

--- MatrixDiagonalDifference.java
+++ MatrixDiagonalDifference.java
@@ -1,25 +1,55 @@
+package com.practice.office.matrix;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
  * 	Authored by PUSHPINDER SINGH 
  * 	psthejohn@gmail.com
  */
+
 public class MatrixDiagonalDifference {
 
 	@SuppressWarnings("resource")
 	public void enterMatrix() {
 
 		// Inputs number of rows of a Matrix from user
+		int a = 0, b = 0;
 		Scanner input1 = new Scanner(System.in);
-		System.out.println("Enter number of rows");
-		int rowNumber = input1.nextInt();
-		
-		// Inputs number of columns of a Matrix from user
 		Scanner input2 = new Scanner(System.in);
-		System.out.println("Enter number of columns");
-		int colNumber = input2.nextInt();
 
+		boolean check1 = false;
+		boolean check2 = false;
+
+		// Inputs number of rows of a Matrix from user
+		while (!check1) {
+			try {
+				System.out.println("Enter number of rows");
+				a = input1.nextInt();
+				check1 = true;
+			} catch (InputMismatchException e) {
+				// input1.next();
+				System.out.println("> i Doubt if  '" + input1.next().toUpperCase() + "' is a number, Let's try again ");
+			}
+		}
+
+		// Inputs number of columns of a Matrix from user
+		while (!check2) {
+			try {
+				System.out.println("Enter number of columns");
+				b = input2.nextInt();
+				check2 = true;
+			} catch (InputMismatchException e) {
+				// input2.next();
+				System.out.println("> i Doubt if  '" + input2.next().toUpperCase() + "' is a number, Let's try again ");
+			}
+		}
+		int rowNumber = a;
+		int colNumber = b;
 		int matrix[][] = new int[rowNumber][colNumber];
+
+		System.out.println("\n" + " You have created a " + rowNumber + "x" + colNumber + " Matrix" + "\n");
+		System.out.println("Now Let's Input Values");
 
 		// Inputing elements of Matrix
 		for (int i = 0; i < rowNumber; i++) {
@@ -32,20 +62,21 @@ public class MatrixDiagonalDifference {
 		}
 
 		System.out.println("\n" + "---------------------------- Matrix is -------------------------" + "\n");
-		//Displaying Matrix
+		// Displaying Matrix
 		for (int i = 0; i < rowNumber; i++) {
 			for (int j = 0; j < colNumber; j++) {
-				System.out.print(" " +matrix[i][j] + "  ");
+				System.out.print(" " + matrix[i][j] + "  ");
 			}
 			System.out.println("");
 		}
 
 		int sumLeftDiagonalElements = 0;
 		int sumRightDiagonalElements = 0;
-		
-		// Checks if a Square Matrix, Hence Proceeds to Calculate Difference of sum of Diagonal Elements of a Matrix
+
+		// Checks if a Square Matrix, Hence Proceeds to Calculate Difference of sum of
+		// Diagonal Elements of a Matrix
 		if (rowNumber == colNumber) {
-			// Selects Left Diagonal elements in this format --> \  of a Matrix
+			// Selects Left Diagonal elements in this format --> \ of a Matrix
 			for (int i = 0; i < rowNumber; i++) {
 				for (int j = 0; j < colNumber; j++) {
 					// Adds Left Diagonal side of Matrix Elements
@@ -56,8 +87,8 @@ public class MatrixDiagonalDifference {
 				}
 			}
 			// System.out.println("Sum of Left Elements --- " + sumLeftElements);
-			
-			// Selects Right Diagonal elements in this format --> /  of a Matrix
+
+			// Selects Right Diagonal elements in this format --> / of a Matrix
 			for (int i = 0; i < rowNumber; i++) {
 				int temp = 0;
 				for (int j = colNumber - i; j > 0; j--) {
@@ -70,17 +101,19 @@ public class MatrixDiagonalDifference {
 
 			}
 			System.out.print("\n" + "-> Difference is :: ");
-			// Checks to never print a negative value 
+			// Checks to never print a negative value
 			if (sumRightDiagonalElements > sumLeftDiagonalElements) {
 				System.out.println(sumRightDiagonalElements - sumLeftDiagonalElements);
 			} else if (sumRightDiagonalElements < sumLeftDiagonalElements) {
 				System.out.println(sumLeftDiagonalElements - sumRightDiagonalElements);
 			} else if (sumLeftDiagonalElements == sumRightDiagonalElements) {
-				System.out.println(sumLeftDiagonalElements - sumRightDiagonalElements+", Both Diagonal have Equal values");
+				System.out.println(
+						sumLeftDiagonalElements - sumRightDiagonalElements + ", Both Diagonal have Equal values");
 			}
 
 		}
-		// If not a Square Matrix, then can't calculate differece of sum of Diagonal Elements of a Matrix
+		// If not a Square Matrix, then can't calculate differece of sum of Diagonal
+		// Elements of a Matrix
 		else {
 			System.out.println("\n" + "Can Not Calculate Difference/Determinant for Rows not Equal to Columns"
 					+ "  !! Only Valid for Square Matrices !!");
